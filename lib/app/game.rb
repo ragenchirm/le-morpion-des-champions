4ddcb979358b3ca75742780cf41c4d0d95146ef3
ragenchirm <noel.gagnaire@gmail.com>
2019-04-18 17:14:41 +0200

    Partie et scores comptés, ordre des joueurs s'inverse entre chaque partie

--- lib/app/game.rb
+++ lib/app/game.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_reader :board, :player1, :player2, :displayer, :@game_count
+  attr_reader :board, :player1, :player2, :displayer, :game_count, :p1_points, :p2_points, :switch_players
 
   def initialize
     @displayer = Displayer.new
@@ -7,8 +7,11 @@ class Game
     @player1 = Player.new("Premier", 1)
     @player2 = Player.new("Second", 2)
     @displayer.get_symbols(@player1.symbol, @player2.symbol)
-    @game_count=0
+    @p1_points=0
+    @p2_points=0
+    @game_count=1
     @board = Board.new
+    @switch_players = false
   end
 
   def perform
@@ -19,7 +22,16 @@ class Game
     refresh_grid
     game_over(result)
     wanna_replay?
+  end
 
+  def perform_reverse
+    until result = board.check_for_end_of_game
+      player_turn(@player2)
+      player_turn(@player1) unless board.check_for_end_of_game
+    end
+    refresh_grid
+    game_over(result)
+    wanna_replay?
   end
 
   def player_turn(player)
@@ -35,6 +47,8 @@ class Game
   def refresh_grid
     system("clear")
     @displayer.display_grid(board.boxes)
+    puts "Partie n° #{@game_count}"
+    puts "Scores : |#{player1.name} : #{@p1_points}|#{player2.name} : #{@p2_points}|"
   end
 
   def game_over(result)
@@ -49,9 +63,11 @@ class Game
       puts "Vous vous êtes mutuellement coincés."
     elsif result[0] == "P1"
       puts "Avant #{@player2.name} habitait en face du cimetière. Maintenant, il habite en face de chez lui."
+      @p1_points +=1
       return @player1
     else
       puts "Dans ce monde, il y a deux types de personnes. Ceux qui ont un pistolet et ceux qui creusent. #{@player1.name}, il creuse."
+      @p2_points +=1
       return player2
     end
   end
@@ -80,6 +96,7 @@ class Game
   def wanna_replay?
     while true
       puts "Ca en refait une chtite ma biche ? (y/n)"
+      puts "(Par contre on change l'ordre de jeu !)"
       print ">"
       choice = gets.chomp
       if !choice.match?(/^y(es)?$/i) && !choice.match?(/^n(o)?$/i)
@@ -87,10 +104,20 @@ class Game
       end
       break if choice.match?(/^y(es)?$/i) ||choice.match?(/^n(o)?$/i)
     end
+
     if choice.match?(/^y(es)?$/i)
       @board = Board.new
       @game_count += 1
-      perform
+      @switch = !@switch
+      if @switch
+        perform_reverse
+      else
+        perform
+      end
     end
+
   end
+
+
+
 end
